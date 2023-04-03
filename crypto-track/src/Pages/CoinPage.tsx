5d789bf183498dc456f8587b620ccc2e357fe37f
johnyoun <johnyoun28@gmail.com>
2023-04-02 19:37:56 -0700

    necessary data displayed not fully styled

--- crypto-track/src/Pages/CoinPage.tsx
+++ crypto-track/src/Pages/CoinPage.tsx
@@ -6,6 +6,20 @@ type CoinData = {
     image: {
         large: string
     }
+    description: {
+        en: string
+    }
+    
+    market_data: {
+        current_price: {
+            usd: number
+        }
+        market_cap: {
+            usd: number
+        }
+    }
+
+    market_cap_rank: number
     name: string
     symbol: string
 }
@@ -31,12 +45,17 @@ const CoinPage = () => {
       
   return (
     <div className='flex items-center text-white-500'>
-        <div className='flex flex-col border-r-2 w-[40%] mt-6'>
+        <div className='flex flex-col border-r-2 w-[30%] mt-6'>
             {
                 <div className='w-full flex flex-col justify-center items-center'>
-                    <img src={coins?.image.large} />
-                    {coins?.name}
-                    {coins?.symbol}
+                    <img className='h-48 mb-5' src={coins?.image.large} />
+                    <h2 className='text-5xl mb-5 font-bold'>{coins?.name}</h2>
+                    <p className='w-11/12 '>{coins?.description.en.split('.')[0]}.</p>
+                    <div className='w-11/12 mt-5'>
+                        <h4>RANK: {coins?.market_cap_rank}</h4>
+                        <h4>CURRENT PRICE: ${coins?.market_data.current_price.usd.toLocaleString()}</h4>
+                        <h4>MARKET CAP: ${coins?.market_data.market_cap.usd.toLocaleString()}</h4>
+                    </div>
                 </div>
         
             }
