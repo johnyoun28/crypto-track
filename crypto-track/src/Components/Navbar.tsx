34fc4cbc435bbe03cde03e8e40c9fc6dc1d7276b
johnyoun <johnyoun28@gmail.com>
2023-04-17 20:39:51 -0700

    add navigate to homepage from coin info

--- crypto-track/src/Components/Navbar.tsx
+++ crypto-track/src/Components/Navbar.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+
+  const navigate = useNavigate()
+  
   return (
     <div className=' w-full flex justify-even pl-[24px] items-center min-h-[64px] border-b-2 border-white-500'>
 
-        <span className='text-[#EEBC1D] font-black text-lg'>Crypto</span>
+        <span onClick={() => navigate('/')} className='text-[#EEBC1D] font-black text-lg cursor-pointer'>Crypto</span>
     </div>
   )
 }
